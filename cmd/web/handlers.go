eb52fdba8e7d4662500eea087e515e916c58d142
JerryLegend254 <jerryokuto713@gmail.com>
2024-08-08 15:11:53 +0300

    feat: added create snippet form validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/JerryLegend254/snippetbox/pkg/models"
 	"github.com/go-chi/chi/v5"
@@ -53,6 +55,37 @@ func (a *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	title := r.PostForm.Get("title")
 	content := r.PostForm.Get("content")
 	expires := r.PostForm.Get("expires")
+
+	errors := make(map[string]string)
+	// Check that the title field is not blank and is not more than 100 characte
+	// long. If it fails either of those checks, add a message to the errors
+	// map using the field name as the key.
+
+	if strings.TrimSpace(title) == "" {
+		errors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		errors["title"] = "This field is too long (maximum is 100 characters)"
+	}
+
+	// Check that the Content field isn't blank.
+	if strings.TrimSpace(content) == "" {
+		errors["content"] = "This field cannot be blank"
+	}
+
+	// Check the expires field isn't blank and matches one of the permitted
+	// values ("1", "7" or "365").
+	if strings.TrimSpace(expires) == "" {
+		errors["expires"] = "This field cannot be blank"
+	} else if expires != "365" && expires != "7" && expires != "1" {
+		errors["expires"] = "This field is invalid"
+	}
+
+	// If there are any errors, dump them in a plain text HTTP response and ret
+	// from the handler.
+	if len(errors) > 0 {
+		fmt.Fprint(w, errors)
+		return
+	}
 
 	id, err := a.snippets.Insert(title, content, expires)
 	if err != nil {
