cbacbc949bafb455c399eb7b9e84c3c6cc20da66
JerryLegend254 <jerryokuto713@gmail.com>
2024-08-14 17:01:26 +0300

    feat: added authorization to relevant routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,15 +16,15 @@ func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Get("/", dynamicMiddleware.ThenFunc(app.home).(http.HandlerFunc))
-	mux.Post("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet).(http.HandlerFunc))
+	mux.Post("/snippet/create", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.createSnippet).(http.HandlerFunc))
 	mux.Get("/snippet/{id}", dynamicMiddleware.ThenFunc(app.showSnippet).(http.HandlerFunc))
-	mux.Get("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippetForm).(http.HandlerFunc))
+	mux.Get("/snippet/create", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.createSnippetForm).(http.HandlerFunc))
 
 	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm).(http.HandlerFunc))
 	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser).(http.HandlerFunc))
 	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm).(http.HandlerFunc))
 	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser).(http.HandlerFunc))
-	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser).(http.HandlerFunc))
+	mux.Post("/user/logout", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.logoutUser).(http.HandlerFunc))
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
