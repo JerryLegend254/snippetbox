15d8b5d576738f7f1ff038a516ac03e5f59be651
JerryLegend254 <jerryokuto713@gmail.com>
2024-08-16 12:19:48 +0300

    feat: added authenticate middleware with context value for user request

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -7,6 +7,7 @@ import (
 	"runtime/debug"
 	"time"
 
+	"github.com/JerryLegend254/snippetbox/pkg/models"
 	"github.com/justinas/nosurf"
 )
 
@@ -60,6 +61,10 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 	buf.WriteTo(w)
 }
 
-func (app *application) authenticatedUser(r *http.Request) int {
-	return app.session.GetInt(r, "userID")
+func (app *application) authenticatedUser(r *http.Request) *models.User {
+	user, ok := r.Context().Value(contextKeyUser).(*models.User)
+	if !ok {
+		return nil
+	}
+	return user
 }
