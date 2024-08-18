58ac1913401da7ccd16e14ab98ec0b197ba9f7ca
JerryLegend254 <jerryokuto713@gmail.com>
2024-08-18 15:01:27 +0300

    feat: added cookies and redirections to e2e tests

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
 )
@@ -27,6 +28,17 @@ type testServer struct {
 // of our custom testServer type.
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts.Client().Jar = jar
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
 	return &testServer{ts}
 }
 
