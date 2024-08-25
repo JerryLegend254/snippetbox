fc6b2f764b972efa4877f16b9a2ccec857b74389
JerryLegend254 <jerryokuto713@gmail.com>
2024-08-25 21:21:57 +0300

    feat: code clean up

--- pkg/models/mysql/testdata/setup.sql
+++ pkg/models/mysql/testdata/setup.sql
@@ -1,3 +1,4 @@
+-- Step 1: Create the snippets table
 CREATE TABLE snippets (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(100) NOT NULL,
@@ -5,7 +6,11 @@ CREATE TABLE snippets (
     created DATETIME NOT NULL,
     expires DATETIME NOT NULL
 );
+
+-- Step 2: Create the index on the created column of the snippets table
 CREATE INDEX idx_snippets_created ON snippets(created);
+
+-- Step 3: Create the users table
 CREATE TABLE users (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -13,10 +18,14 @@ CREATE TABLE users (
     hashed_password CHAR(60) NOT NULL,
     created DATETIME NOT NULL
 );
+
+-- Step 4: Add a unique constraint on the email column of the users table
 ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
+
+-- Step 5: Insert a sample user
 INSERT INTO users (name, email, hashed_password, created) VALUES (
     'Alice Jones',
     'alice@example.com',
     '$2a$12$NuTjWXm3KKntReFwyBVHyuf/to.HEwTy.eS206TNfkGfr6HzGJSWG',
     '2018-12-23 17:25:22'
-    );
+);
